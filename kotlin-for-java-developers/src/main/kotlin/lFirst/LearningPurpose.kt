a1cb5eb62591c596ada88dcdfd670db2d6bb5a83
talibnabi <talibnabiyev@gmail.com>
2023-03-11 14:39:19 +0400

    adding some classes for learning purpose

--- kotlin-for-java-developers/src/main/kotlin/lFirst/LearningPurpose.kt
+++ kotlin-for-java-developers/src/main/kotlin/lFirst/LearningPurpose.kt
@@ -1,6 +1,23 @@
 package lFirst
 
+
 class LearningPurpose {
+    private var name: String = "Alisa"
+    private var surname: String = "Aslan"
+    private var age: Int = 22
+
+    fun getName(): String {
+        return name
+    }
+
+    fun getSurname(): String {
+        return surname
+    }
+
+    fun getAge(): Int {
+        return age
+    }
+
     fun main(array: Array<String>) {
         forExample()
     }
--- kotlin-for-java-developers/src/main/kotlin/lFirst/LearningPurposeMain.kt
+++ kotlin-for-java-developers/src/main/kotlin/lFirst/LearningPurposeMain.kt
@@ -1,8 +1,16 @@
 package lFirst
 
-fun main(array: Array<String>){
+fun main(args: Array<String>) {
     val forExample = LearningPurpose().forExample()
     println(forExample)
+    val s = args[0]
+    val ss = args[1]
+    val sss = args[2]
+    println(s + " " + ss + " " + sss)
+    val age = LearningPurpose().getAge()
+    val name = LearningPurpose().getName()
+    val surname = LearningPurpose().getSurname()
+    println("name: $name,surname: $surname,age: $age")
 }
 
 
